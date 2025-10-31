af346cf9e74dc160f2c39f0680c9f7fc854c7d12
waqas123123916 <waqas@cosmicsync.co>
2025-10-31 21:01:04 +0500

    Add initial project setup with components, assets, and styles
    - Created main application structure with App and Navbar components.
    - Added navigation links in a constants file.
    - Implemented basic styling using Tailwind CSS and custom fonts.
    - Included various assets such as SVGs, images, and videos for features.
    - Configured Vite for React and Tailwind CSS integration.

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+                    {label: 'Store', href: '#'},
+                    {label: 'Mac', href: '#'},
+                    {label: 'iPhone', href: '#'},   
+                    {label: 'iPad', href: '#'},
+                    {label: 'Watch', href: '#'},
+                    {label: 'AirPods', href: '#'},
+                ]
