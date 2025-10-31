da6360d74f58d39fc81406caf75878d830b4775d
waqas123123916 <waqas@cosmicsync.co>
2025-10-31 23:02:31 +0500

    feat: add ProductViewer component with 3D Macbook customization
    - Updated package.json to include dependencies for react-three and zustand.
    - Created ProductViewer component for displaying a customizable 3D Macbook.
    - Integrated color and size controls using Zustand for state management.
    - Added Canvas with Box and OrbitControls for 3D rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 const App = () => {
   return (
     <div className=' '>
         <Navbar/>
         <Hero/>
+        <ProductViewer/>
     </div>
   )
 }
---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import useMacbookStore from '../store';
+import clsx from 'clsx';
+import { Box, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+const ProductViewer = () => {
+    const {color,scale,setColor,setScale}=useMacbookStore();
+  return (
+   <section id='product-viewer'>
+    <h2>Take a closer look.</h2>
+  <div className="controls">
+      <p className='info'>
+        MacbookPro {scale} in {color}
+    </p>
+
+    <div className="flex-center gap-5 mt-">
+        <div className="color-control">
+            <div onClick={()=>setColor('#adb5bd')} className={clsx('bg-neutral-300',color === '#adb5bd' && 'active')}/>
+            <div onClick={()=>setColor('#2e2c2e')} className={clsx('bg-neutral-900',color === '#2e2c2e' && 'active')}/>
+        </div>
+        <div className="size-control">
+            <div onClick={()=>setScale(0.06)} className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent')} ><p>14"</p></div>
+            <div onClick={()=>setScale(0.08)} className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent')} ><p>16"</p></div>
+        </div>
+    </div>
+  </div>
+  <Canvas id="canvas" camera={{position:[0,2,5], fov:50, near:0.1,far:100}}>
+    <Box position={[0,0,0]} scale={10*scale} material-color={color} ></Box>
+    <OrbitControls enableZoom={false} />
+  </Canvas>
+   </section>
+  )
+}
+
+export default ProductViewer
